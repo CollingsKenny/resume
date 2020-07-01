5d89a56d6dce65791bc19d91e2630e0d944fc7ce
Kenny <keacolli@ucsc.edu>
2020-06-30 22:34:00 -0700

    add input params to build script

--- build.js
+++ build.js
@@ -1,12 +1,22 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+const args = process.argv.slice(2);
+if (args.length != 1) {
+  console.error('invalid usage: build.js FILE');
+  return 0;
+}
+
+const fileName = args[0];
+
+console.log(fileName);
+
 var showdown = require('showdown'),
   converter = new showdown.Converter();
 converter.setFlavor('github');
 var fs = require('fs');
 
-const html_content = converter.makeHtml(fs.readFileSync('resume.md', 'utf8'));
+const html_content = converter.makeHtml(fs.readFileSync(fileName, 'utf8'));
 
 JSDOM.fromFile('template.html').then((dom) => {
   dom.window.document.querySelector('main').innerHTML = html_content;
